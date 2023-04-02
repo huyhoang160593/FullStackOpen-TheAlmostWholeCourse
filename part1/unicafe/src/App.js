1fd278e461bf8fe7e42501973c33f382601aeb20
Huy Hoang <huyhoang160593@yahoo.com>
2023-04-02 17:46:04 +0700

    done 1-10

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,17 @@
 import { useState } from "react";
 
+const Button = ({ title, onClick }) => {
+  return <button onClick={onClick}>{title}</button>;
+};
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <p>
+      {text} {value}
+    </p>
+  );
+};
+
 const Statistics = (props) => {
   const all = props.good + props.neutral + props.bad;
   const average = (props.good - props.bad) / all;
@@ -7,12 +19,12 @@ const Statistics = (props) => {
 
   return (
     <>
-      <p>good {props.good}</p>
-      <p>neutral {props.neutral}</p>
-      <p>bad {props.bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive}</p>
+      <StatisticLine text="good" value={props.good} />
+      <StatisticLine text="neutral" value={props.neutral} />
+      <StatisticLine text="bad" value={props.bad} />
+      <StatisticLine text="all" value={all} />
+      <StatisticLine text="average" value={average} />
+      <StatisticLine text="positive" value={positive} />
     </>
   );
 };
@@ -26,13 +38,15 @@ const App = () => {
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button onClick={() => setGood(good + 1)} title="good" />
+      <Button onClick={() => setNeutral(neutral + 1)} title="neutral" />
+      <Button onClick={() => setBad(bad + 1)} title="bad" />
 
       <h1>statistics</h1>
-      {(good + neutral + bad) === 0 && (<p>No feedback given</p>)}
-      {(good + neutral + bad) !== 0 && (<Statistics good={good} neutral={neutral} bad={bad}/>)}
+      {good + neutral + bad === 0 && <p>No feedback given</p>}
+      {good + neutral + bad !== 0 && (
+        <Statistics good={good} neutral={neutral} bad={bad} />
+      )}
     </div>
   );
 };
