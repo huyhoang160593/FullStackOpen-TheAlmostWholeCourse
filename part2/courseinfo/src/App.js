c9e03702239d9cf82a89e4961c538931ac2826dc
Huy Hoang <huyhoang160593@yahoo.com>
2023-04-04 01:12:49 +0700

    done 2-4

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,5 +1,5 @@
 const Header = ({ name }) => {
-  return <h1> {name} </h1>;
+  return <h2> {name} </h2>;
 };
 
 const Part = ({ name, exercises }) => {
@@ -11,7 +11,10 @@ const Part = ({ name, exercises }) => {
 };
 
 const Content = ({ parts }) => {
-  const totalExercise = parts.reduce((accumulate, current) => accumulate + current.exercises, 0)
+  const totalExercise = parts.reduce(
+    (accumulate, current) => accumulate + current.exercises,
+    0
+  );
   return (
     <>
       {parts.map((part) => (
@@ -22,44 +25,67 @@ const Content = ({ parts }) => {
   );
 };
 
-const Course = ({ course }) => {
+const Course = ({ courses }) => {
   return (
     <main>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
+      <h1>Web development curriculum</h1>
+      {courses.map((course) => (
+        <div key={course.id}>
+          <Header name={course.name} />
+          <Content parts={course.parts} />
+        </div>
+      ))}
     </main>
   );
 };
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-      {
-        name: "Redux",
-        exercises: 11,
-        id: 4,
-      },
-    ],
-  };
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
-  return <Course course={course} />;
+  return <Course courses={courses} />;
 };
 
 export default App;
