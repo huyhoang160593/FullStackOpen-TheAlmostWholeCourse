99ae86da0219a957db60b7cda7893a4ef7ecf6e5
Huy Hoang <huyhoang160593@yahoo.com>
2023-04-04 22:45:45 +0700

    done 2-6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const onNameChangeHandle = (event) => {
+    event.preventDefault();
+    setNewName(event.target.value);
+  };
+
+  const onNameSubmitHandle = (event) => {
+    event.preventDefault();
+    setPersons([...persons, { name: newName }]);
+    setNewName("");
+  };
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={onNameSubmitHandle}>
+        <div>
+          name: <input onChange={onNameChangeHandle} value={newName} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
+    </div>
+  );
+};
+
+export default App;
