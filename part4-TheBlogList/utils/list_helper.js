ca48a7a38cd0371244c22fbb96946d163cc4321e
Huy Hoang <huyhoang160593@yahoo.com>
2023-04-13 12:55:37 +0700

    done 4-6 without Lodash

--- part4-TheBlogList/utils/list_helper.js
+++ part4-TheBlogList/utils/list_helper.js
@@ -8,8 +8,27 @@ const favoriteBlog = (blogs) => {
   return blogs.find((blog) => blog.likes === mostLikeValue);
 };
 
+const mostBlog = (blogs) => {
+  if (!blogs || blogs.length === 0) return null;
+  const countBlogPerAuthor = blogs.reduce((blogPerAuthor, blog) => {
+    if (!blogPerAuthor[blog.author]) {
+      blogPerAuthor[blog.author] = 1;
+      return blogPerAuthor;
+    }
+    blogPerAuthor[blog.author] += 1;
+    return blogPerAuthor;
+  }, {});
+  const mostBlogValue = Math.max(...Object.values(countBlogPerAuthor));
+  const authorWithMostBlog = Object.entries(countBlogPerAuthor).map((entry) => ({
+    author: entry[0],
+    blogs: entry[1],
+  })).find((entry) => entry.blogs === mostBlogValue);
+  return authorWithMostBlog;
+};
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlog,
 };
