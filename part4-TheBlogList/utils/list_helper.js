15fd0980951aab1d1d9828925d2f14bd9772e36d
Huy Hoang <huyhoang160593@yahoo.com>
2023-04-13 11:49:21 +0700

    done 4-5

--- part4-TheBlogList/utils/list_helper.js
+++ part4-TheBlogList/utils/list_helper.js
@@ -2,7 +2,14 @@ const dummy = (_blogs) => 1;
 
 const totalLikes = (blogs) => blogs.reduce((sumLikes, blog) => sumLikes + blog.likes, 0);
 
+const favoriteBlog = (blogs) => {
+  if (!blogs || blogs.length === 0) return null;
+  const mostLikeValue = Math.max(...blogs.map((blog) => blog.likes));
+  return blogs.find((blog) => blog.likes === mostLikeValue);
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
