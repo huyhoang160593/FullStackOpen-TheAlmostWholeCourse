2ce57a2772d4f1c041ceec1cc6c70e1ba0c504be
Huy Hoang <huyhoang160593@yahoo.com>
2023-04-16 14:57:27 +0700

    done 4-10

--- part4-TheBlogList/tests/blog_api.spec.js
+++ part4-TheBlogList/tests/blog_api.spec.js
@@ -28,6 +28,16 @@ test('new blog have unique identifier property name id', () => {
   expect(newBlogObject.id).toBeDefined();
 });
 
+test('post new blog successful', async () => {
+  await api
+    .post('/api/blogs')
+    .send(helper.newBlogObject)
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+  const blogInDB = await helper.blogsInDB();
+  expect(blogInDB.length).toEqual(helper.initialBlogs.length + 1);
+});
+
 afterAll(async () => {
   await mongoose.connection.close();
 });
