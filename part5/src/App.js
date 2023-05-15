dc89583c1af6b36d13520020e7286511555a7fac
Huy Hoang <huyhoang160593@yahoo.com>
2023-05-15 16:09:35 +0700

    done 5-1

--- part5/src/App.js
+++ part5/src/App.js
@@ -1,24 +1,31 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import { useState, useEffect } from 'react';
+import Blog from './components/Blog';
+import blogService from './services/blogs';
+import LoginForm from './components/LoginForm';
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    if (!user) return
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, [user]);
 
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
+      {!user && <LoginForm setUser={setUser} />}
+      {user && (
+        <>
+          <h2>blogs</h2>
+          <p>{user.username} logged in</p>
+          {blogs.map((blog) => (
+            <Blog key={blog.id} blog={blog} />
+          ))}
+        </>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ part5/src/components/LoginForm.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import loginService from "./../services/login"
+import blogsService from "./../services/blogs"
+
+const LoginForm = ({ setUser }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  /** @type {React.FormEventHandler<HTMLFormElement>} */
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.getAll({
+        username, password
+      })
+      setUser(user)
+      blogsService.setToken(user.token)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      // TODO: add exception later
+    }
+  }
+
+  return (
+    <form onSubmit={handleLogin}>
+      <h2>log in to application</h2>
+      <div>
+        username
+        <input type="text" name="Username" value={username} onChange={({target}) => setUsername(target.value)}/>
+      </div>
+      <div>
+        password
+        <input type="password" name="Password" value={password} onChange={({target}) => setPassword(target.value)}/>
+      </div>
+      <button type="submit">login</button>
+    </form>
+  );
+};
+
+export default LoginForm;
