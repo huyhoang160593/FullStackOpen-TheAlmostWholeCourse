18fe7488ed4282a2c53afd482258927cf5cc363e
Huy Hoang <huyhoang160593@yahoo.com>
2023-05-16 16:34:25 +0700

    done 5-10

--- part5/src/App.js
+++ part5/src/App.js
@@ -21,7 +21,7 @@ const defaultNotificationState = {
 };
 
 const App = () => {
-  const [blogs, setBlogs] = useState([]);
+  const [blogs, setBlogs] = useState(/** @type {import('./components/Blog').Blog[]} */([]));
   const [user, setUser] = useState(null);
   const [notification, setNotification] = useState({
     ...defaultNotificationState,
@@ -110,7 +110,7 @@ const App = () => {
             />
           </Togglable>
 
-          {blogs.map((blog) => (
+          {blogs.sort((a,b) => b.likes - a.likes).map((blog) => (
             <Blog key={blog.id} blog={blog} updateBlogList={updateBlogHandle} />
           ))}
         </>
