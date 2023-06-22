c4cb078eeda7a817f551e234c69eecf7ba186cf4
Huy Hoang <huyhoang160593@yahoo.com>
2023-06-23 00:17:09 +0700

    update eslint rules

--- part7/bloglist-extend-react-query-final/.eslintrc.js
+++ part7/bloglist-extend-react-query-final/.eslintrc.js
@@ -40,14 +40,17 @@ module.exports = {
       'never'
     ],
     'eqeqeq': 'error',
-    'no-trailing-spaces': 'error',
     'object-curly-spacing': [
       'error', 'always'
     ],
     'arrow-spacing': [
       'error', { 'before': true, 'after': true }
     ],
+    'no-trailing-spaces': 'error',
     'no-console': 0,
+    'no-unused-vars': [
+      'error', { "argsIgnorePattern": "^_" }
+    ],
     'react/prop-types': 0,
     'react/react-in-jsx-scope': 'off'
   },
