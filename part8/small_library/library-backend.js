0d46b779a0757fc0a0744b6831838cd54b719f6e
Huy Hoang <huyhoang160593@yahoo.com>
2023-07-04 15:39:41 +0700

    done 8-16

--- part8/small_library/library-backend.js
+++ part8/small_library/library-backend.js
@@ -1,104 +1,18 @@
-const { v1: uuid } = require('uuid');
 const { ApolloServer } = require('@apollo/server');
 const { startStandaloneServer } = require('@apollo/server/standalone');
-const { PORT, MONGODB_URI } = require('./utilities/config');
-const { default: mongoose } = require('mongoose');
+const {
+  PORT,
+  MONGODB_URI,
+  SECRET_PASSWORD,
+  JWT_SECRET,
+} = require('./utilities/config');
+const { default: mongoose, Mongoose } = require('mongoose');
+const jwt = require('jsonwebtoken');
 const Book = require('./models/Book');
 const Author = require('./models/Author');
 const { GraphQLError } = require('graphql');
+const User = require('./models/User');
 mongoose.set('strictQuery', false);
-
-let authors = [
-  {
-    name: 'Robert Martin',
-    id: 'afa51ab0-344d-11e9-a414-719c6709cf3e',
-    born: 1952,
-  },
-  {
-    name: 'Martin Fowler',
-    id: 'afa5b6f0-344d-11e9-a414-719c6709cf3e',
-    born: 1963,
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: 'afa5b6f1-344d-11e9-a414-719c6709cf3e',
-    born: 1821,
-  },
-  {
-    name: 'Joshua Kerievsky', // birthyear not known
-    id: 'afa5b6f2-344d-11e9-a414-719c6709cf3e',
-  },
-  {
-    name: 'Sandi Metz', // birthyear not known
-    id: 'afa5b6f3-344d-11e9-a414-719c6709cf3e',
-  },
-];
-
-/*
- * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- *
- * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
- *
- * Spanish:
- * Podría tener más sentido asociar un libro con su autor almacenando la id del autor en el contexto del libro en lugar del nombre del autor
- * Sin embargo, por simplicidad, almacenaremos el nombre del autor en conección con el libro
- */
-
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: 'afa5b6f4-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring'],
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: 'afa5b6f5-344d-11e9-a414-719c6709cf3e',
-    genres: ['agile', 'patterns', 'design'],
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: 'afa5de00-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring'],
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: 'afa5de01-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring', 'patterns'],
-  },
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: 'afa5de02-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring', 'design'],
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: 'afa5de03-344d-11e9-a414-719c6709cf3e',
-    genres: ['classic', 'crime'],
-  },
-  {
-    title: 'The Demon ',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: 'afa5de04-344d-11e9-a414-719c6709cf3e',
-    genres: ['classic', 'revolution'],
-  },
-];
 
 /*
   you can remove the placeholder query once your first own has been implemented
@@ -114,6 +28,14 @@ mongoose
   });
 
 const typeDefs = `#graphql
+  type User {
+    username:      String!
+    favoriteGenre: String!
+    id:            ID!
+  }
+  type Token {
+    value: String!
+  }
   type Book {
     title:     String!
     published: Int!
@@ -132,6 +54,7 @@ const typeDefs = `#graphql
     authorCount:                             Int
     allBooks(author: String, genre: String): [Book!]!
     allAuthors:                              [Author!]!
+    me:                                      User
   }
   type Mutation {
     addBook(
@@ -144,8 +67,23 @@ const typeDefs = `#graphql
       name:      String!
       setBornTo: Int!
     ): Author
+    createUser(
+      username: String!
+      favoriteGenre: String!
+    ): User
+    login(
+      username: String!
+      password: String!
+    ): Token
   }
 `;
+
+/**
+ * @typedef {object} User
+ * @property {string} username
+ * @property {string} favoriteGenre
+ * @property {import('mongoose').ObjectId} id
+ */
 
 const resolvers = {
   Query: {
@@ -158,22 +96,22 @@ const resolvers = {
      */
     allBooks: async (_root, { author, genre }) => {
       if (author) {
-        const existAuthor = await Author.findOne({name: author})
-        if(!existAuthor) return []
+        const existAuthor = await Author.findOne({ name: author });
+        if (!existAuthor) return [];
         return Book.find({
           author: existAuthor._id,
           ...(genre && { genres: genre }),
-        }).populate('author', { name: 1, born: 1 })
+        }).populate('author', { name: 1, born: 1 });
       }
       return Book.find({
         ...(genre && { genres: genre }),
       }).populate('author', { name: 1, born: 1 });
     },
     allAuthors: async () => Author.find({}),
+    me: (_root, _args, context) => context.currentUser,
   },
   Author: {
-    bookCount: (root) =>
-      books.filter((book) => book.author === root.name).length,
+    bookCount: (root) => 0,
   },
   Mutation: {
     /**
@@ -183,8 +121,16 @@ const resolvers = {
      *  published: number
      *  genres:    string[]
      * }} args
+     * @param {{ currentUser?: User }} context
      * */
-    addBook: async (_root, args) => {
+    addBook: async (_root, args, context) => {
+      if (!context.currentUser) {
+        throw new GraphQLError('wrong credentials', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+          },
+        });
+      }
       let existAuthor = await Author.findOne({
         name: args.author,
       });
@@ -220,13 +166,59 @@ const resolvers = {
      *  name: string,
      *  setBornTo: number
      * }} args
+     * @param {{ currentUser?: User }} context
      * */
-    editAuthor: async (_root, args) => {
+    editAuthor: async (_root, args, context) => {
+      if (!context.currentUser) {
+        throw new GraphQLError('wrong credentials', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+          },
+        });
+      }
       return Author.findOneAndUpdate(
         { name: args.name },
         { born: args.setBornTo },
         { new: true, runValidators: true, context: 'query' }
       );
+    },
+    /**
+     *
+     * @param {*} root
+     * @param {{ username: string, favoriteGenre: string }} args
+     */
+    createUser: async (_root, args) => {
+      const user = new User(args);
+      return user.save().catch((error) => {
+        throw new GraphQLError('Creating the user failed', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            error,
+          },
+        });
+      });
+    },
+    /**
+     *
+     * @param {*} _root
+     * @param {{ username: string, password: string }} args
+     */
+    login: async (_root, args) => {
+      const user = await User.findOne({ username: args.username });
+      if (!user || args.password !== SECRET_PASSWORD) {
+        throw new GraphQLError('wrong credentials', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+          },
+        });
+      }
+
+      const userForToken = {
+        username: user.username,
+        id: user._id,
+      };
+
+      return { value: jwt.sign(userForToken, JWT_SECRET) };
     },
   },
 };
@@ -238,6 +230,14 @@ const server = new ApolloServer({
 
 startStandaloneServer(server, {
   listen: { port: PORT },
+  context: async ({ req, res:_res }) => {
+    const auth = req ? req.headers.authorization : null;
+    if (auth && auth.startsWith('Bearer ')) {
+      const decodeToken = jwt.verify(auth.substring(7), JWT_SECRET);
+      const currentUser = await User.findById(decodeToken.id);
+      return { currentUser };
+    }
+  },
 }).then(({ url }) => {
   console.log(`Server ready at ${url}`);
 });
--- part8/small_library/utilities/config.js
+++ part8/small_library/utilities/config.js
@@ -2,8 +2,12 @@ require('dotenv').config()
 
 const PORT = process.env.PORT
 const MONGODB_URI = process.env.MONGODB_URI
+const JWT_SECRET = process.env.JWT_SECRET
+const SECRET_PASSWORD = process.env.SUPER_SECRET_GLOBAL_PASSWORD
 
 module.exports = {
   PORT,
-  MONGODB_URI
+  MONGODB_URI,
+  JWT_SECRET,
+  SECRET_PASSWORD
 }
