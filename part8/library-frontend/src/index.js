cf666cd890588b8e4dc43ae5cb3a06b1403cb2a3
The 99's Puppycat <huyhoang160593@yahoo.com>
2023-08-27 22:49:33 +0700

    some refactoring

--- part8/library-frontend/src/index.js
+++ part8/library-frontend/src/index.js
@@ -1,32 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { LocalStorageKeys } from 'constants/localStorageKeys';
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem(LocalStorageKeys.LIBRARY_USER_TOKEN)
+  const token = localStorage.getItem(LocalStorageKeys.LIBRARY_USER_TOKEN);
   const newHeaders = {
     ...headers,
-    ...(token && ({authorization: `Bearer ${token}`}))
-  }
+    ...(token && { authorization: `Bearer ${token}` }),
+  };
   return {
-    headers: newHeaders
-  }
-})
+    headers: newHeaders,
+  };
+});
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000',
-})
+});
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: authLink.concat(httpLink)
-})
+  link: authLink.concat(httpLink),
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-<ApolloProvider client={client}>
-  <App />
-</ApolloProvider>
-)
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
